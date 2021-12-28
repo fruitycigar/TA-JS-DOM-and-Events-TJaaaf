cf7d3b7b9076ca6b6f36d4f102524638d291e1e7
fruitycigar <shruthisagar@SHODAN.local>
2021-12-28 19:24:06 +0530

    Done

--- block-BJaael/code/script.js
+++ block-BJaael/code/script.js
@@ -1,9 +1,14 @@
+// 1. Create 500 boxes of size (4rem and 4rem)
+// 2. Each box will contain a random number from 0 - 500
+// 3. When moving the mouse over the boxes the background color of each box should change to some random color.
+// 4. Along with random color the random number should also change.
+
 function randomNumber(max) {
     return Math.floor(Math.random() * max);
 }
 
-function getRandomColor() {
-    let hexcodeArr = [
+function randomColor() {
+    let hexcodeCharacters = [
         '0',
         '1',
         '2',
@@ -20,24 +25,21 @@ function getRandomColor() {
         'd',
         'e',
         'f',
-    ];
-
+    ]
     let color = "#";
 
-    for (let i = 0; i < 6; i++) {
-        color = color + hexcodeArr[randomNumber(16)];
+    for(let i = 0; i < 6; i++) {
+        color = color + hexcodeCharacters[randomNumber(16)];
     }
 
     return color;
 }
 
-
 let parentBox = document.querySelector(".boxes");
-
 
 for (let i = 0; i < 500; i++) {
     let div = document.createElement("div");
-    div.classList.add("box");
+    div.classList.add("box")
 
     let h3 = document.createElement("h3");
     h3.innerText = randomNumber(500);
@@ -49,9 +51,9 @@ for (let i = 0; i < 500; i++) {
 let allBoxes = document.querySelectorAll(".box");
 
 function handleMouseMove() {
-    allBoxes.forEach((box) => {
-        box.style.backgroundColor = getRandomColor();
-        box.querySelector('h3').innerText = randomNumber(500);
+    allBoxes.forEach((elm) => {
+        elm.style.backgroundColor = randomColor();
+        elm.querySelector('h3').innerText = randomNumber(500);
     })
 }
 
