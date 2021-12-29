c6ad6b330051cb186fbc1072a1f1cb2734536a0f
fruitycigar <shruthisagar@SHODAN.local>
2021-12-29 15:39:02 +0530

    I will never take flash games for granted ever again

--- block-BJaaep/code/script.js
+++ block-BJaaep/code/script.js
@@ -1,1 +1,21 @@
-// I am a noob, and thus, I am looking at the videos yet again to understand what's going on.
+let firstBoxes = document.querySelectorAll(".first li");
+
+firstBoxes.forEach((box, index) => {
+    box.addEventListener('click', (event) => {
+        event.target.innerText = index + 1;
+
+        setTimeout(() => {            
+            event.target.innerText = "";
+        }, 5000)
+    })
+})
+
+let secondBox = document.querySelector(".second");
+
+secondBox.addEventListener('click', (event) => {
+    event.target.innerText = Number(event.target.dataset.text);
+
+    setTimeout(() => {
+        event.target.innerText = ""; 
+    }, 5000)
+})
